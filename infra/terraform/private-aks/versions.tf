5d09060e8f82035341179aa198cc638aeacb1701
Alejandra Guillen <algut@microsoft.com>
2020-04-09 22:06:38 -0500

    testing not specifying backend in versions file

--- infra/terraform/private-aks/versions.tf
+++ infra/terraform/private-aks/versions.tf
@@ -1,10 +1,11 @@
 terraform {
   required_version = ">= 0.12" 
   backend "azurerm" {
-      storage_account_name = "__terraformstorageaccount__"
-      container_name       = "terraform"
-      key                  = "terraform.tfstate"
-      access_key  ="__storagekey__"
+    #   storage_account_name = "__terraformstorageaccount__"
+    #   container_name       = "terraform"
+    #   key                  = "terraform.tfstate"
+    #   access_key  ="__storagekey__" 
+    
   }
 }
 
